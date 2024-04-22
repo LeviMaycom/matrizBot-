27b6770200435165315568fab3a518e2681e92fd
LeviMaycom <167050569+LeviMaycom@users.noreply.github.com>
2024-04-22 18:32:27 -0300

    feat: create ready console event

---
+++ src/events/main/ready.ts
@@ -0,0 +1,17 @@
+import { client } from "../..";
+import { Event } from "../../structs/types/Event";
+
+export default new Event({
+    name: "ready",
+    once: true,
+    run(){
+        const { commands,buttons, selects, modals } = client
+
+        console.log("✅ Bot online".green);
+        console.log(`🔥Commands loaded: ${commands.size}`.blue);
+        console.log(`🔥Buttons loaded: ${buttons.size}`.blue);
+        console.log(`🔥Select loaded: ${selects.size}`.blue);
+        console.log(`🔥Modals loaded: ${modals.size}`.blue);
+
+    },
+})
